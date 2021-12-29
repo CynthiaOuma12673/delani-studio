9fbe6d61bb0716eb22b3328eb17fdd118997b88e
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2021-12-29 09:48:06 +0300

    it will add the toggle style to the icons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
 // toggle effect
 $(document).ready(function(){
-    $('.icon-image').click(function(){
-        $('.paragraph').toggle();
+    $('.design').click(function(){
+            $('.design').toggle();
+                $('#para-one').toggle();
+                $('#para-one').click(function(){
+                    $('.design').toggle();
+                    $('#para-one').hide();
+                    $('.design').show();
+                });
+            });
+
+    $('.dev').click(function(){
+        $('.dev').toggle();
+        $('#para-two').toggle();
+        $('#para-two').click(function(){
+            $('.dev').toggle();
+            $('#para-two').hide();
+            $('.dev').show();
+        });
+    });
+
+    $('.product').click(function(){
+        $('.product').toggle();
+        $('#para-three').toggle();
+        $('#para-three').click(function(){
+            $('.product').toggle();
+            $('#para-three').hide();
+            $('.product').show();
+        });
     });
 });
