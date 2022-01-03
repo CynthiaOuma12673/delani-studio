0c705e70e6d1241ded5f866c04b6ee6b7910c244
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-01-03 10:26:51 +0300

    it will adjust the images in portfolio and create hover effect

--- js/index.js
+++ js/index.js
@@ -31,3 +31,78 @@ $(document).ready(function(){
     });
 });
 
+// hover effect
+$(document).ready(function(){
+    $('#port1').mouseover(function(){
+        $('#proj1').show();
+    });
+    $('#port1').mouseout(function(){
+        $('#proj1').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port2').mouseover(function(){
+        $('#proj2').show();
+    });
+    $('#port2').mouseout(function(){
+        $('#proj2').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port3').mouseover(function(){
+        $('#proj3').show();
+    });
+    $('#port3').mouseout(function(){
+        $('#proj3').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port4').mouseover(function(){
+        $('#proj4').show();
+    });
+    $('#port4').mouseout(function(){
+        $('#proj4').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port5').mouseover(function(){
+        $('#proj5').show();
+    });
+    $('#port5').mouseout(function(){
+        $('#proj5').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port6').mouseover(function(){
+        $('#proj6').show();
+    });
+    $('#port6').mouseout(function(){
+        $('#proj6').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port7').mouseover(function(){
+        $('#proj7').show();
+    });
+    $('#port7').mouseout(function(){
+        $('#proj7').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#port8').mouseover(function(){
+        $('#proj8').show();
+    });
+    $('#port8').mouseout(function(){
+        $('#proj8').hide();
+    });
+});
+
+// forms validation
+
